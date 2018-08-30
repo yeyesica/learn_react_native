c9d82f29ab0a283aff88f54d7dacb4c6612d58f8
yeyesica <yesica.adelia@gmail.com>
2018-08-30 15:49:47 +0700

    react_native_components

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
+import Judul from './Components/Judul'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>Nama: Yesica Adelia Ramadani </Text>
-        <Text style={styles.instructions}>Kelas: XI_RPL 4 </Text>
-        <Text style={styles.instructions}>No.Absen: 34 </Text>
+      <View>
+        <Judul></Judul>
+        <Text>Nama: Yesica Adelia Ramadani </Text>
+        <Text>Kelas: XI_RPL 4 </Text>
+        <Text>No.Absen: 34 </Text>
         <Image style={{width: 400, height:500}}
         source={require('./1.jpg')}
         />
